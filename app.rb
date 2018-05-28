ba4085b3bd8d3ef5de9b15a7f56c38a315da9186
Ally Netzel <netzelally@gmail.com>
2018-05-28 14:53:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,9 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
   get '/reversename/:name' do
-    @rev = params[:name.reverse]
+    @rev = params[:name].reverse
     "#{@rev}"
   end
 end
