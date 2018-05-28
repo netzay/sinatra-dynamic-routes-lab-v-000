2c9684508a9cc53ebc3aec674c10044303b062ed
Ally Netzel <netzelally@gmail.com>
2018-05-28 15:37:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,6 +18,6 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
      @say = params[:word1]+params[:word2]+params[:word3]+params[:word4]+params[:word5]
-     "#{@say}."
+      " ""#{@say}."
   end
 end
