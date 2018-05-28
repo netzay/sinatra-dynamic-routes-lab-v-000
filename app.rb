e796a92af4d049e0d670050d53fa0d0385bdbcd6
Ally Netzel <netzelally@gmail.com>
2018-05-28 16:00:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,16 +6,16 @@ class App < Sinatra::Base
     @rev = params[:name].reverse
     "#{@rev}"
   end
-  
+
   get '/square/:number' do
     @square = params[:number].to_i ** 2
     "#{@square}"
   end
-  
+
   get '/say/:number/:phrase' do
     params[:phrase] * params[:number].to_i
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     words = []
     words[0] = params[:word1]
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     phrase_string.chop! << "."
     "#{phrase_string}."
   end
-  
+
   get '/:operation/:number1/:number2' do
     case params[:operation]
     when "+"
@@ -41,7 +41,7 @@ class App < Sinatra::Base
       "#{params[:number1].to_i * params[:number2].to_i}"
     when "/"
       "#{params[:number1].to_i / params[:number2].to_i}"
-    when "-"
+    else 
       "#{params[:number2].to_i - params[:number1].to_i}"
     end
   end
