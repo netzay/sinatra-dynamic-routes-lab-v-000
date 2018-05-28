f65935b4160e025eceff655074ea89d6f3020049
Ally Netzel <netzelally@gmail.com>
2018-05-28 15:06:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @say = params[:number].times do params[:phrase]
+    @say = params[:number].times{params[:phrase]}
       "#{@say}"
     end
   end
