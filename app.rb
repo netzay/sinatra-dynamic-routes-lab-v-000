b5cd24ccc2ab01f1c970dcf28217b94ea339f112
Ally Netzel <netzelally@gmail.com>
2018-05-28 15:41:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,6 +34,6 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @add = params[:number1].to_i + params[:number2].to_i
+    params[:number1].to_i + params[:number2].to_i
   end
 end
